21265da35c571eec61b4f7dfdaebf5b014943ac2
doublenhan <doublenhan@users.noreply.github.com>
2020-05-09 11:49:12 +0700

    Create User.cs
    user 1

---
+++ WebCK/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebCK.Models
+{
+    public class user
+    { 
+        [Key]
+        public int ID { get; set; }
+        
+        [StringLength (50)]
+        [Display(Name ="Họ Tên")]
+        public string FullName { get; set; }
+
+        [StringLength(9)]
+        [Display(Name = "Mã Số Sinh Viên")]
+        public string MSSV { get; set; }
+
+        [StringLength(9)]
+        [Display(Name = "Lớp")]
+        public string Classs { get; set; }
+
+        [StringLength(9)]
+        [Display(Name = "Địa Chỉ")]
+        public string Address { get; set; }
+    }
+}
